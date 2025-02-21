ed11aad38b86a0c58f76dad17517450efcdf1fe7
0xflotus <0xflotus@gmail.com>
2025-02-21 14:08:19 +0100

    feat: added berlin clock functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,110 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
 
 export default function Home() {
+  const [now, setNow] = useState(new Date());
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const firstRow = Math.floor(hours / 5);
+  const secondRow = hours % 5;
+  const thirdRow = Math.floor(minutes / 5);
+  const fourthRow = minutes % 5;
+
+  setInterval(() => setNow(new Date()), 1000);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <h1>Berlin Clock</h1>
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+            <circle r="45" cx="50" cy="50" fill={seconds % 2 != 0 ? "grey" : "red"} />
+          </svg> 
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={firstRow / 1 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={firstRow / 2 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={firstRow / 3 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={firstRow / 4 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={secondRow / 1 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={secondRow / 2 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={secondRow / 3 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={secondRow / 4 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 1 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 2 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 3 >= 1 ? "red" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 4 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 5 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 6 >= 1 ? "red" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 7 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 8 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 9 >= 1 ? "red" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 10 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="50" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="50" height="100" x="10" y="10" rx="20" ry="20" fill={thirdRow / 11 >= 1 ? "yellow" : "grey"} />
+          </svg>
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={fourthRow / 1 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={fourthRow / 2 >= 1 ? "yellow" : "grey"} />
+          </svg> 
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={fourthRow / 3 >= 1 ? "yellow" : "grey"} />
+          </svg>
+          <svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={fourthRow / 4 >= 1 ? "yellow" : "grey"} />
+          </svg> 
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
     </div>
   );
